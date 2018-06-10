69cda0ec05eeb0976169721b8379448b69766d64
chalat chansima <chalat.csm@gmail.com>
2018-06-10 09:39:31 +0700

    Init Project File

---
+++ app/src/main/java/com/chalat/surveys/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.chalat.surveys
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .add(R.id.contentContainer, MainFragment.newInstance())
+                    .commit()
+        }
+    }
+}
